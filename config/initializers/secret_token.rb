8a8347c807dbb041f5cc973440d7775cd855ca6b
Neilson Maknarakham <nmaknarakham@gmail.com>
2013-03-11 11:25:15 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CahApp::Application.config.secret_token = '05f6abc2f4d37a4ed47fbf945608530f533091dde62cea6a4f15c7157071d65de9aee3c195c0c23441bbe03ce9304bda9f212064d8c915d5be453c8c83330ea1'
